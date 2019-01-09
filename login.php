f1aca6639e256a9c003a8a1191efec879eaceeb5
marcelvandertuin <aurumrun@gmail.com>
2019-01-09 12:29:31 +0100

    login

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">	
+		<title>Login</title>
+	</head>
+	<body>
+		<h1>Login</h1>
+		<form name="formLogin" method="post" action="">
+			<input type="text" name="username" placeholder="username" />
+			<input type="password" name="password" placeholder="password" />
+			<input type="submit" name="submitLogin" value="login" />
+		</form>
+		<?php
+		if(isset($_POST['submitLogin'])){
+			$username = $_POST['username'];
+			$password = $_POST['password'];
+		}
+		?>
+	</body>
+</html>
