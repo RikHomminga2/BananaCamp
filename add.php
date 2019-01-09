b43c212712e548f8590671732d978df7d61556d6
annabelkrijgsman <annabelkrijgsman@gmail.com>
2019-01-09 15:33:07 +0100

    added tabs

--- add.php
+++ add.php
@@ -1,27 +1,26 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title></title>
-
-<?php
-	include "addQuestion.php";
-?>
-
-
-</head>
-<body>
-
-<form method="post">
-	<p>Add question:</p>
-	<input type="text" name="question"><br/>
+	<head>
+		<title></title>
 	
-	<p>Add answers:</p>
-	<input type="text" name="answer1"><br/>
-	<input type="text" name="answer2"><br/>
-	<input type="text" name="answer3"><br/>
-	<input type="text" name="answer4"><br/><br/>
-	<button>Save</button>
-</form>
+		<?php
+			include "addQuestion.php";
+		?>
 
-</body>
+	</head>
+	<body>
+
+		<form method="post">
+			<p>Add question:</p>
+			<input type="text" name="question"><br/>
+			
+			<p>Add answers:</p>
+			<input type="text" name="answer1"><br/>
+			<input type="text" name="answer2"><br/>
+			<input type="text" name="answer3"><br/>
+			<input type="text" name="answer4"><br/><br/>
+			<button>Save</button>
+		</form>
+
+	</body>
 </html>
