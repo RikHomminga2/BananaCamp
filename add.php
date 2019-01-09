cfc404af718354479e5c61fc2fe55c68a0d2ab76
annabelkrijgsman <annabelkrijgsman@gmail.com>
2019-01-09 12:26:58 +0100

    answer fields

--- add.php
+++ add.php
@@ -2,12 +2,24 @@
 <html>
 <head>
 	<title></title>
+
+<?php
+	include "addQuestion.php";
+?>
+
+
 </head>
 <body>
 
-<form>
+<form method="post">
 	<p>Add question:</p>
-	<input type="text"><br/>
+	<input type="text" name="question"><br/>
+	
+	<p>Add answers:</p>
+	<input type="text" name="answer1"><br/>
+	<input type="text" name="answer2"><br/>
+	<input type="text" name="answer3"><br/>
+	<input type="text" name="answer4"><br/><br/>
 	<button>Save</button>
 </form>
 
