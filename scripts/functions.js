51e5f3c87e96c7493fb6c10ad565a65de3906469
Kaj Huisman <kaj.huisman@gmail.com>
2019-01-11 21:21:08 +0100

    Refactoring scripts/functions.js and includes/header.php

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,27 +1,16 @@
 const fstatus = (response) => {
-		return (response.status == 200) ? Promise.resolve(response) : Promise.reject(new Error(response.statusText));
-	}
-	
+	return (response.status == 200) ? Promise.resolve(response) : Promise.reject(new Error(response.statusText));
+}
 const json = (response) => {
 	return response.json(); 
 }
-
 const ferror = (error) => {
 	console.log('Request failed: ', error);
 }
-const fetchForm = (inputForm) => {
-	fetch(inputForm).then(fstatus).then(function(response) {
+const fetchHTML = (url) => {
+	fetch(url).then(fstatus).then(function(response) {
 		return response.text();
 	}).then(function(data) {
 		document.querySelector('main').innerHTML = data;
 	}).catch(ferror);
 }
-const getLoginForm = () => {
-	let inputForm = 'login.html';
-	fetchForm(inputForm);
-}
-const getRegisterForm = () => {
-	let inputForm = 'register.html';
-	fetchForm(inputForm);
-}
-
