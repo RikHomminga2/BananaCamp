bd9650613ceafbdff8299ab77aad7a597099b6fe
Kaj Huisman <kaj.huisman@gmail.com>
2019-01-11 13:42:26 +0100

    adding encrypted passwords

--- main.php
+++ main.php
@@ -4,7 +4,7 @@
 	if($_SERVER['REQUEST_METHOD'] == 'POST') {
 		$request = $_POST['request'];
 		$un = $_POST['username'];
-		$pw = $_POST['password'];
+		$pw = hash('sha512', $_POST['password']);
 		if($request == 'login') {
 			login($un, $pw);
 		}
