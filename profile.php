35ba7995278455df004853f57dc5e2f7a3c1c342
annabelkrijgsman <annabelkrijgsman@gmail.com>
2019-01-11 16:13:52 +0100

    added basic grid

--- profile.php
+++ profile.php
@@ -1,11 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">	
-		<title>Profile</title>
-	</head>
-	<body>
-		<h1>Profile</h1>
-	</body>
-</html>
+<?php
+	include 'includes/header.php'
+?>
+<link rel="stylesheet" type="text/css" href="includes/style.css" />
+<main>
+	<div class="grid-container">
+		<div class="grid-item1">
+			<h3>Profile image + Name</h3>
+			1
+		</div>
+		<div class="grid-item2">
+			<h3>Badges</h3>
+			2
+		</div>
+		<div class="grid-item3">
+			<h3>About Student</h3>
+			3
+		</div>
+		<div class="grid-item4">
+			<h3>Statistics</h3>
+			4
+		</div>  
+		<div class="grid-item5">
+			<h3>Links</h3>
+			5
+		</div>
+		<div class="grid-item6">
+			<h3>Personal Details</h3>
+			6
+		</div>
+		<div class="grid-item7">
+			<h3>Connection to other students</h3>
+			7
+		</div>  
+	</div>
+</main>
+<?php
+	include 'includes/footer.php'
+?>
