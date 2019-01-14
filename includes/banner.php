0315dbdae4b4f8acf17a4201dc73e123629c31c7
annabelkrijgsman <annabelkrijgsman@gmail.com>
2019-01-14 15:12:18 +0100

    recover mistake

--- includes/banner.php
+++ includes/banner.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8">
 		<!--<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
 		<script src="scripts/functions.js"></script>
-		<link rel="stylesheet" href="includes/styletest.css">
+		<link rel="stylesheet" href="includes/style.css">
 		<title>Bananacamp</title>
 	</head>
     <header>
