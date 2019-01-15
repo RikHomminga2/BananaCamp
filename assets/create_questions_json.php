123ee5debb56730f48afaab448e6f7d22efffe6e
annabelkrijgsman <annabelkrijgsman@gmail.com>
2019-01-15 15:30:47 +0100

    removing unneeded files

--- assets/create_questions_json.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$stager = array();
-	$f = fopen('questions.txt', 'r');
-	while(!feof($f)) {
-		array_push($stager, explode(',', fgets($f)));
-	}
-	echo json_encode($stager);
-		
-		
--- assets/login_function.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-var_dump($_POST['username']);
-
-//$token = array();
-//$token['id'] = $id;
-//$token['username'] = $_POST['username'];
-//echo JWT::encode($token, 'secret_server_key');
-?>
