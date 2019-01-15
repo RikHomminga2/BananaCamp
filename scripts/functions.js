581784ad3c5c62116a80d48bc2d9cdde0253685c
Kaj Huisman <kaj.huisman@gmail.com>
2019-01-15 15:10:30 +0100

    Adding questions functionality

--- scripts/functions.js
+++ scripts/functions.js
@@ -14,3 +14,37 @@ const fetchHTML = (url) => {
 		document.querySelector('main').innerHTML = data;
 	}).catch(ferror);
 }
+
+function fetchQuestions() {
+	let post = {
+		method: 'post',
+		headers: {
+			"Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+		},
+		body: 'request=getQuestions'
+	}
+	fetch('main.php', post).then(fstatus).then(json).then(createQuestionsForm).catch(ferror);
+}
+
+function createQuestionsForm(obj) {
+	let main = document.getElementById('getQuestions');
+	let frm = document.createElement('form');
+	frm.setAttribute('method', 'post');
+	frm.setAttribute('action', 'main.php');
+	main.appendChild(frm);
+	for(x of obj){
+		let ipt = document.createElement('input');
+		let lbl = document.createElement('label');
+		let br = document.createElement('br');
+		ipt.setAttribute('type', 'checkbox');
+		ipt.setAttribute('id', `question${x.id}`);
+		lbl.setAttribute('for', `question${x.id}`);
+		lbl.innerText = x.question +'?';
+		frm.appendChild(ipt);
+		frm.appendChild(lbl);
+		frm.appendChild(br);
+	}
+	let ipt = document.createElement('input');
+	ipt.setAttribute('type', 'submit');
+	frm.appendChild(ipt);
+}
