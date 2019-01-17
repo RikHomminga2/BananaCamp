7484c201bb2537ec71dd519d51868e325054c623
Kaj Huisman <kaj.huisman@gmail.com>
2019-01-17 12:46:34 +0100

    small changes to header file

--- includes/header.php
+++ includes/header.php
@@ -5,13 +5,16 @@
 		<!--<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
 		<script src="scripts/functions.js"></script>
 		<link rel="stylesheet" href="styles/style.css">
+		<link rel="stylesheet" href="styles/experimental.css">
 		<title>Bananacamp</title>
 	</head>
 	<body>
-		<ul class="sidenav">
-			<button><a href="index.php">BananaCamp</a></button>
-			<button onclick="fetchHTML('register.html')">Register</button>
-			<button onclick="fetchHTML('login.html')">Login</button>
-			<!-- <button><a href="">Contact</a></button> -->
-			<button><a href="#">About</a></button>     
-		</ul>
+		<header>
+			<nav>
+				<button><a href="index.php">BananaCamp</a></button>
+				<button onclick="fetchHTML('html/register.html')">Register</button>
+				<button onclick="fetchHTML('html/login.html')">Login</button>
+				<!-- <button><a href="">Contact</a></button> -->
+				<button onclick="fetchHTML('html/about.html')">About</a></button>
+			</nav>
+		</header>
