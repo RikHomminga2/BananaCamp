39eccd8749449cbdce037d2c9146e98bda860012
annabelkrijgsman <annabelkrijgsman@gmail.com>
2019-01-17 16:35:47 +0100

    added styling

--- includes/header.php
+++ includes/header.php
@@ -4,8 +4,9 @@
 		<meta charset="utf-8">
 		<!--<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
 		<script src="scripts/functions.js"></script>
-		<link rel="stylesheet" href="styles/style.css">
-		<link rel="stylesheet" href="styles/experimental.css">
+		<link rel="stylesheet" href="styles/style2.css">
+		<!--<link rel="stylesheet" href="styles/experimental.css">-->
+		<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
 		<title>Bananacamp</title>
 	</head>
 	<body>
--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php require('includes/header.php');?>
-		<main>
-			<h1>BananaCamp</h1>
-			<p>
-				Het idee is een webapplicatie te maken die het beheren/managen van een bootcamp eenvoudiger maakt.
-				Het PRIMARAIRE doel is een project ontwikkelen dat als showcase voor de vaardigheden van de teamleden kan dienen zodat eventuele werkgevers daar inzicht in kunnen krijgen.
-				Het SECUNDAIRE doel is een project ontwikkelen dat productiegereed is en in de markt gezet kan worden.
-			</p>
-		</main>
+	<main class="index">
+		<img src="https://i.imgur.com/gIxOyTc.jpg" alt="" class="logoindex">
+	</main>
 <?php require('includes/footer.php');?>
