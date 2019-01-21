75eeb00de04c8f26d080f2d7887c29e9a965a684
annabelkrijgsman <annabelkrijgsman@gmail.com>
2019-01-21 13:48:16 +0100

    exams page

---
+++ exams.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title>Exams</title>
+		<script src="scripts/functions2.js"></script>
+		<script src="https://d3js.org/d3.v5.min.js"></script>
+		<script>
+			document.addEventListener("DOMContentLoaded", fetchExam);
+		</script>
+	</head>
+	<body>
+		<main>
+			<section id="getExams">
+				<h2>Exams:</h2>
+			</section>
+		</main>
+	</body>
+</html>
